c5f78d4ace5991bf928616814f7a31db3ef4db06
picroc <picroc@yandex-team.ru>
2022-01-25 23:10:04 +0300

    Change project 2

---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import AnimationLogo from './components/animationLogo.svelte';
+	import AnimationScroller from './components/animationScroller.svelte';
+
+	export let name: string;
+</script>
+
+<main>
+	<AnimationScroller />
+	<AnimationLogo />
+	<p>This is a placeholder for the LIGHT project.</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
